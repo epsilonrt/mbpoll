74e31d02ce4c1ebddff6ebeba01163613e7ddd90
Pascal JEAN <pascal.jean@btssn.net>
2015-08-11 16:29:35 +0200

    Mise à jour doc et numéro version

--- package/win/mbpoll.iss
+++ package/win/mbpoll.iss
@@ -1,7 +1,7 @@
 ; Copyright © 2015 Pascal JEAN aka epsilonRT
 ; All rights reserved.
 
-#define VERSION "0.1-6"
+#define VERSION "0.1-10"
 [Setup]
 ; NOTE: The value of AppId uniquely identifies this application.
 ; Do not use the same AppId value in installers for other applications.
--- version-git.h
+++ version-git.h
@@ -1,6 +1,6 @@
-#define VERSION "0.1-8-gbe55c2f"
-#define VERSION_SHORT "0.1-8"
+#define VERSION "0.1-10-gae51499"
+#define VERSION_SHORT "0.1-10"
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 1
-#define VERSION_PATCH 8
-#define VERSION_SHA1 0xbe55c2f
+#define VERSION_PATCH 10
+#define VERSION_SHA1 0xae51499
