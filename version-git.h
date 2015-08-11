e909e1b0228af478a0afe32199b8082f072865e8
Pascal JEAN <pascal.jean@btssn.net>
2015-08-11 15:29:11 +0200

    mise à jour numéro de version

--- version-git.h
+++ version-git.h
@@ -1,6 +1,6 @@
-#define VERSION "0.1-6-g2e684c8"
-#define VERSION_SHORT "0.1-6"
+#define VERSION "0.1-8-gbe55c2f"
+#define VERSION_SHORT "0.1-8"
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 1
-#define VERSION_PATCH 6
-#define VERSION_SHA1 0x2e684c8
+#define VERSION_PATCH 8
+#define VERSION_SHA1 0xbe55c2f
